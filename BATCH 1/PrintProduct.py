fab9c515308e99bcc0089cc44bcd965587791c99
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 16:54:01 +0800

    Update and rename Prog04.py to PrintProduct.py

---
+++ BATCH 1/PrintProduct.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
+print(num1*num2)
