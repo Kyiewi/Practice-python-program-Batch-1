b3d096c0eb362d7c0e802b8015bb250b01db5221
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 16:51:07 +0800

    Update and rename Prog02.py to PrintEqual.py

--- BATCH 1/PrintEqual.py
+++ BATCH 1/PrintEqual.py
@@ -1,8 +1,8 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
-z = int(input("Input first number: "))
-i = int(input("Input second number: "))
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
 
-if z==i:
+if num1==num2:
     print("Equal")
 
