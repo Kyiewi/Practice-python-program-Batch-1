3907ca4693c35d6a910fefb47620871970ac007f
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 16:50:26 +0800

    Update PrintBigNum.py

--- BATCH 1/PrintBigNum.py
+++ BATCH 1/PrintBigNum.py
@@ -1,8 +1,8 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-x = int(input("Input first number: "))
-y = int(input("Input second number: "))
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
 
-if x > y:
-    print(x)
+if num1 > num2:
+    print(num1)
 else:
-    print(y)
+    print(num2)
